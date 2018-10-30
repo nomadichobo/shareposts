704d6340bb73e61c0c71179c2545bdeec51f1301
Martin Svensson <nomadichobo.com@gmail.com>
2018-10-30 18:51:40 +0100

    First Commit

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    // DB Params
+    define ('DB_HOST', 'localhost');
+    define ('DB_USER', '_YOUR_USER');
+    define ('DB_PASS', '_YOUR_PASS');
+    define ('DB_NAME', '_YOUR_DB_NAME');
+    
+    // App Root
+    define('APPROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URLROOT', '_YOUR_URL_');
+    // Site Name
+    define('SITENAME', '_YOUR_SITENAME');
